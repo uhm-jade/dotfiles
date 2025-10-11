8cb936d1a07ca44b75cdc536b0e000e4759e671d
jade bandy <uhm.jade@gmail.com>
2025-10-11 03:54:01 -0600

    add install script

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ here just take em
 ## install
 `python3 install.py`
 
-
+see `config.toml` for file list
--- build.py
+++ build.py
@@ -7,21 +7,19 @@ from pathlib import Path
 home = Path(os.path.expanduser("~"))
 script = Path(os.path.abspath(__file__))
 
-repo = script.parent.parent # look a roblox reference
+repo = script.parent # look a roblox reference
 
 with open(str(script.parent / "config.toml"), "rb") as f:
     cfg = tomllib.load(f)
 
 is_debug = cfg["build"]["debug"]
-home_paths = cfg["build"]["files"]
+home_paths = cfg["files"]
 
 destination = repo
 if is_debug:
-    destination = repo / "test"
+    destination = repo / "test-build"
 
 destination.mkdir(parents=True, exist_ok=True)
-
-# print("home_directory is " + home_directory)
 
 for partial_path in home_paths:
     assert not Path(partial_path).is_absolute(), f"Absolute path not allowed: {partial_path}"
@@ -33,7 +31,4 @@ for partial_path in home_paths:
         shutil.copytree(str(path), str(joined_path), dirs_exist_ok=True)
     else:
         shutil.copy(str(path), str(destination))
-
-
-# subprocess.run(["ls", "-l", home_directory + paths[0]])
 
